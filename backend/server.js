66b0f6b19d53a86efc100ccecc19f832b40001e7
Rishika12U <151054846+Rishika12U@users.noreply.github.com>
2025-08-05 00:30:54 +0530

    Update CORS for Netlify frontend

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,24 @@ const app = express();
 const leaderboardRoutes = require('./routes/leaderboardRoutes.js');
 const internRoutes = require('./routes/internRoutes.js');
 
-app.get('/test', (req, res) => res.send('Test route works!'));
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      'http://localhost:5173',
+      'https://rainbow-licorice-b37739.netlify.app'
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'), false);
+    }
+  },
+  credentials: true,
+};
 
-app.use(cors());
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use('/api/leaderboard', leaderboardRoutes);
 app.use('/api/intern', internRoutes);
 
